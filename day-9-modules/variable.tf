822a5369424d13a1c8e0e359e5325d0fd6ed8e09
sravani <marrisravanireddy04@gmail.com>
2024-04-02 15:00:27 +0530

    dat-9-modules updates

---
+++ day-9-modules/variable.tf
@@ -0,0 +1,13 @@
+variable "ami_id" {
+    description = "passing ami value to main.tf"
+    type = string
+    default = ""
+}
+variable "instance_type" {
+    type = string
+    default = ""
+}
+variable "key_name" {
+    type = string
+    default = ""
+}
